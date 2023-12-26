cc1ebc0789b6974c7d35edf69ca0c730e852a1e2
MdMunnaRahman <146363445+MdMunnaRahman@users.noreply.github.com>
2023-12-26 23:48:56 +0600

    web

---
+++ CSE/java.js
@@ -0,0 +1,37 @@
+// Swper Js
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Typeed JS 
+var typed = new Typed('#typed', {
+  strings: ['To become a center of excellence in Computer Science and Engineering through quality teaching, lead and transformation of research that contributes to the betteme of sosity. '],
+  typeSpeed: 50,
+  loop: true,
+  loopCount: Infinity,
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
